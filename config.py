a939547b7ff419b372b247aad3e991f21127d6ea
𝙰𝚂𝙷𝙾𝙺𝚇𝙶𝙷𝙾𝚂𝚃 <106798009+AshokChakravarthy666@users.noreply.github.com>
2025-02-17 21:14:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "7925953890:AAFT6mnQPK3E1yqKf3Sr9-GwWdhnevS
 
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002440769217"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002351435664"))
-ADMINS = int(environ.get("ADMINS", "5003133371 6650849235"))
+ADMINS = int(environ.get("ADMINS", "5003133371,6650849235"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://Tgbot:123@cluster0.bz1f9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/")
